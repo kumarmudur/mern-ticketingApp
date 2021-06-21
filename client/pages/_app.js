6975f6b590ae594efc226a1a844871ca35258604
BasavakumarMudur <bmudur@tekion.com>
2021-06-21 07:05:10 +0530

    added other functionality

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,17 +1,26 @@
 import 'bootstrap/dist/css/bootstrap.css';
 
 import buildClient from '../api/build-client';
+import Header from '../components/Header';
 
-const AppComponent = ({ Component, pageProps }) => {
+const AppComponent = ({ Component, pageProps, currentUser }) => {
     return (
         <div>
+            <Header currentUser={currentUser} />
             <Component { ...pageProps }/>
         </div>
     )
 };
 
-AppComponent.getInitialProps = () => {
+AppComponent.getInitialProps = async (appContext) => {
+    const client = buildClient(appContext.ctx);
+    const  { data } = await client.get('/api/users/currentuser');
 
+    let pageProps = {};
+    if (appContext.Component.getInitialProps) {
+        pageProps = appContext.Component.getInitialProps(appContext.ctx);
+    }
+    return { pageProps, ...data, currentUser: data.currentUser };
 };
 
 export default AppComponent;
