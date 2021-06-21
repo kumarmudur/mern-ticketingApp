8aca7155a6fddddfe5fda53cc92b8c95931b1db6
BasavakumarMudur <bmudur@tekion.com>
2021-06-21 07:57:56 +0530

    added tickets configuaration

---
+++ tickets/src/app.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+import 'express-async-errors';
+import { json } from 'body-parser';
+import cookieSession from 'cookie-session';
+
+import { errorHandler } from './middlewares/error-handler';
+import { NotFoundError } from './errors/not-found-error';
+
+const app = express();
+app.set('trust proxy', true);
+app.use(json());
+app.use(cookieSession({ signed: false, secure: process.env.NODE_ENV !== 'test'}));
+
+app.all('*', async () => { throw new NotFoundError()});
+app.use(errorHandler);
+
+export { app };
